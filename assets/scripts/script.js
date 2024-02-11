e49bfbda99424f2b056ba529a1ee07d8fcce77f3
Kiwu <maxnight2014@gmail.com>
2024-02-11 22:18:49 +0200

    Add the final touches to the project
    Added all the functionality to the project. Make
    it somewhat responsive with almost every screen.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,16 +1,39 @@
 let container = document.querySelector('.container')
-let grid = 10;
-let gridSize = grid * grid;
+width = container.offsetWidth
+let grid = prompt('How large do you want your canvas to be? [max 100]');
+if (grid > 100) {
+    grid = 100
+}
+let gridSize = grid * grid
+
+function generateGrid(){
+// Generates a grid
+for(i=0; i< gridSize; i++) {
+    div = document.createElement('div')
+    div.setAttribute("class", `div_${i}`)
+    container.appendChild(div) 
+    let currentDiv = container.querySelector(`.div_${i}`)
+    currentDiv.addEventListener('mouseover', ()=> {
+        if(rainbowMode == 1 && mouseDown == 1) {
+            currentDiv.style.background = `${getRandomHexColor(getRandomHexColor)}`
+        } else if (mouseDown == 1 && rainbowMode == 0) {
+            currentDiv.style.background = `${color}`
+        }
+    }) 
+}
+}
+generateGrid()
 let inputColor = document.querySelector('#colorPicker');
 let color = 'black';
+let rainbowMode = 0;
 function getColor() {
     inputColor.addEventListener('input', ()=> {
         color = inputColor.value
-        console.log(color)
     })
 }
 getColor()
 
+// Verifies if click is being pressed down or not
 var mouseDown = 0;
 document.body.onmousedown = function() { 
   mouseDown = 1;
@@ -19,22 +42,37 @@ document.body.onmouseup = function() {
   mouseDown = 0;
 }
 
-for(i=0; i< gridSize; i++) {
-    div = document.createElement('div')
-    div.setAttribute("class", `div_${i}`)
-    container.appendChild(div) 
-    let currentDiv = container.querySelector(`.div_${i}`)
-    currentDiv.addEventListener('mouseover', ()=> {
-        if(mouseDown == 1)
-            currentDiv.style.background = `${color}`
-    }) 
+function currentColor() {
+    rainbowMode = 0;
+    color = inputColor.value
 }
-
 
 let styleRoot = document.querySelector(":root")
 function gridRowSize() {
-    styleRoot.style.setProperty('--width', `${500 / grid}px`)
+    styleRoot.style.setProperty('--width', `${width / grid}px`)
 }
-
 gridRowSize()
 
+// Adds eraser function by changing the background color to white
+function eraser() {
+    rainbowMode = 0;
+    color = 'white'
+}
+function clearCanvas() {
+  for(i=0; i< gridSize; i++) {
+    container.children[i].style.background = 'white'
+  }
+}
+
+function getRandomHexColor() {
+    const letters = '0123456789ABCDEF';
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++) {
+      hexColor += letters[Math.floor(Math.random() * 16)];
+    }
+    return color = hexColor;
+  }
+
+function rainbowModeOn() {
+    rainbowMode = 1;
+}

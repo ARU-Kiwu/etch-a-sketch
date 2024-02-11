2ef61450cfc3b7d3489c6cbebf0837f5cdeece5a
Kiwu <maxnight2014@gmail.com>
2024-02-11 15:51:29 +0200

    Make the design and color picker
    Made the basic responsive design structure
    with a color picker mode and basic functionality.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,22 +1,40 @@
-let grid = prompt("Enter a number less than 100")
-let gridSize = grid * grid
-let container = document.querySelector(".container")
-let div
+let container = document.querySelector('.container')
+let grid = 10;
+let gridSize = grid * grid;
+let inputColor = document.querySelector('#colorPicker');
+let color = 'black';
+function getColor() {
+    inputColor.addEventListener('input', ()=> {
+        color = inputColor.value
+        console.log(color)
+    })
+}
+getColor()
+
+var mouseDown = 0;
+document.body.onmousedown = function() { 
+  mouseDown = 1;
+}
+document.body.onmouseup = function() {
+  mouseDown = 0;
+}
+
 for(i=0; i< gridSize; i++) {
     div = document.createElement('div')
     div.setAttribute("class", `div_${i}`)
     container.appendChild(div) 
     let currentDiv = container.querySelector(`.div_${i}`)
     currentDiv.addEventListener('mouseover', ()=> {
-        currentDiv.style.background = 'white'
-    })
+        if(mouseDown == 1)
+            currentDiv.style.background = `${color}`
+    }) 
 }
 
 
 let styleRoot = document.querySelector(":root")
 function gridRowSize() {
-    styleRoot.style.setProperty('--width', `${grid * 10}px`)
+    styleRoot.style.setProperty('--width', `${500 / grid}px`)
 }
 
 gridRowSize()
-console.log(container.children)
+
